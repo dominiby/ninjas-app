f77d0d706a38badce0e7d1f203c61c3aa82b3a2e
Dominik Bysko <dominik.bysko@billennium.com>
2019-03-08 13:24:17 +0100

    Updates

---
+++ precache-manifest.8fd2d1e2358c6f6ee26fc888834ed599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4c0b2ec39d49975fbe22380a15e1bdf",
+    "url": "/ninjas-app/static/media/logo.e4c0b2ec.jpg"
+  },
+  {
+    "revision": "39195042a487d7b84817",
+    "url": "/ninjas-app/static/js/runtime~main.39195042.js"
+  },
+  {
+    "revision": "ef21a23239f5d5712265",
+    "url": "/ninjas-app/static/js/main.ef21a232.chunk.js"
+  },
+  {
+    "revision": "9bdbde35308c8e4b1334",
+    "url": "/ninjas-app/static/js/1.9bdbde35.chunk.js"
+  },
+  {
+    "revision": "ef21a23239f5d5712265",
+    "url": "/ninjas-app/static/css/main.5980e5e1.chunk.css"
+  },
+  {
+    "revision": "14416324f26507c1a842e8db4a317719",
+    "url": "/ninjas-app/index.html"
+  }
+];
